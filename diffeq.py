cfb3b5daf16463fc0a458977eab16c828949bae1
Maede Seyedolmohadesin <MaedeMohadesin@Maedes-MacBook-Pro.local>
2018-09-28 19:27:53 -0400

    Create Challenge problem 2.1 and 2.2

--- diffeq.py
+++ diffeq.py
@@ -4,6 +4,9 @@ Created on Fri Sep 21 16:42:19 2018
 
 @author: Tom K
 """
+#%matplotlib inline
+import numpy as np
+from matplotlib import pyplot 
 
 def rk2(func,a=0,b=10,N=20):
     h = (b-a)/N
@@ -27,10 +30,10 @@ def rk2(func,a=0,b=10,N=20):
 def rk4(func,a=0,b=10,N=20):
     h = (b-a)/N
 
-    tpoints = arange(a,b,h)
+    tpoints = np.arange(a,b,h)
     xpoints = []
 
-    x = 0.0
+    x = 10.0
     for t in tpoints:
         xpoints.append(x)
         k1 = h*func(x,t)
@@ -39,8 +42,10 @@ def rk4(func,a=0,b=10,N=20):
         k4 = h*func(x+k3,t+h)
         x += (k1+2*k2+2*k3+k4)/6
 
-    pyl.plot(tpoints,xpoints)
-    pyl.xlabel("t")
-    pyl.ylabel("x(t)")
-    pyl.show()
-    return xpoints
+    #pyplot.plot(tpoints,xpoints)
+    #pyplot.xlabel("t")
+    #pyplot.ylabel("x(t)")
+    #pyplot.show()
+    #print('h is',h)
+    return (xpoints ,tpoints)
+
